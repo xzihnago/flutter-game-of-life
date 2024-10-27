9322315b1cdbf0a261e8cb94a1ec88110689425b
Xzihnago <xzihnago@gmail.com>
2024-10-27 17:49:02 +0800

    Add randomize button

--- lib/game_of_life.dart
+++ lib/game_of_life.dart
@@ -4,12 +4,7 @@ class GameOfLife {
   final int size;
   final List<List<bool>> grid = [];
 
-  GameOfLife(this.size) {
-    set(List.generate(
-      size,
-      (_) => List.generate(size, (_) => Random().nextBool()),
-    ));
-  }
+  GameOfLife(this.size);
 
   Stream<List<List<bool>>> stream(Duration duration) async* {
     while (true) {
@@ -32,7 +27,7 @@ class GameOfLife {
 
       for (var x = 0; x < size; x++) {
         final alive = grid[y][x];
-        final neighbors = checkNeighbors(y, x);
+        final neighbors = _checkNeighbors(y, x);
 
         if (alive && (neighbors == 2 || neighbors == 3)) {
           next[y].add(true);
@@ -47,7 +42,14 @@ class GameOfLife {
     set(next);
   }
 
-  int checkNeighbors(int i, int j) {
+  void randomize() => set(List.generate(
+        size,
+        (_) => List.generate(size, (_) => Random().nextBool()),
+      ));
+
+  bool _checkBoundary(int value) => value >= 0 && value < size;
+
+  int _checkNeighbors(int i, int j) {
     var count = 0;
 
     for (final offset in [
@@ -63,13 +65,11 @@ class GameOfLife {
       final x = i + offset[0];
       final y = j + offset[1];
 
-      if (isInBound(x) && isInBound(y) && grid[x][y]) {
+      if (_checkBoundary(x) && _checkBoundary(y) && grid[x][y]) {
         count++;
       }
     }
 
     return count;
   }
-
-  bool isInBound(int value) => value >= 0 && value < size;
 }
--- lib/home.dart
+++ lib/home.dart
@@ -6,19 +6,34 @@ class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(child: stream()),
-      );
+  Widget build(BuildContext context) {
+    final game = GameOfLife(32);
+    game.randomize();
 
-  StreamBuilder<List<List<bool>>> stream() => StreamBuilder(
-        stream: GameOfLife(32).stream(const Duration(milliseconds: 100)),
-        builder: (context, snapshot) => CustomPaint(
-          size: const Size(400, 400),
-          painter: GameOfLifePainter(
-            snapshot.data ?? [],
-            4,
-            Theme.of(context).textTheme.bodyMedium!.color!,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: StreamBuilder(
+              stream: game.stream(const Duration(milliseconds: 100)),
+              builder: (context, snapshot) => CustomPaint(
+                size: const Size(400, 400),
+                painter: GameOfLifePainter(
+                  snapshot.data ?? [],
+                  4,
+                  Theme.of(context).textTheme.bodyMedium!.color!,
+                ),
+              ),
+            ),
           ),
-        ),
-      );
+          const SizedBox(height: 64),
+          ElevatedButton(
+            onPressed: game.randomize,
+            child: const Text('Randomize'),
+          ),
+        ],
+      ),
+    );
+  }
 }
